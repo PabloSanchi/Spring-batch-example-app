01847a618f4b00f1b0ac6d8a98789e77d0f4c8a8
Pablo Sanchidrian <pablo.sanchi.herrera@gmail.com>
2024-08-19 18:54:50 +0200

    feat: billing job app setup

---
+++ src/main/java/example/billing_job/BillingJobConfiguration.java
@@ -0,0 +1,8 @@
+package example.billing_job;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BillingJobConfiguration {
+    // TODO add job definition here
+}
